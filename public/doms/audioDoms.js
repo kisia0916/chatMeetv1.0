6006bd957f8617ddbe31ceeece576e141fced2ac
kisia0916 <fumiharuabe@gmail.com>
2023-08-25 00:13:50 +0900

    audio none

--- public/doms/audioDoms.js
+++ public/doms/audioDoms.js
@@ -4,23 +4,23 @@ const audioDoms = (userList,userId)=>{
         if(i.userId == userId){
             if(index == 0){
                 return`
-                    <audio id="myAudio" class="firstAudio" audioType = 'ambient' audioCategory = 'playback' autoplay></audio>
+                    <audio id="myAudio" class="firstAudio" audioType = 'ambient' audioCategory = 'playback' style="display:none;" autoplay></audio>
                 `
             }else{
                 return`
-                    <audio id="myAudio" controls disablecontrols></audio>
+                    <audio id="myAudio" controls disablecontrols audioType = 'ambient' audioCategory = 'playback' style="display:none;"></audio>
 
                 `
             }
         }else{
             if(index == 0){
                 return`
-                    <audio id="audio:${i.userId}" class="firstAudio" controls disablecontrols autoplay></audio>
+                    <audio id="audio:${i.userId}" class="firstAudio" controls disablecontrols autoplay audioType = 'ambient' audioCategory = 'playback' style="display:none;"></audio>
 
                 `
             }else{
                 return`
-                    <audio id="audio:${i.userId}" controls disablecontrols autoplay></audio>
+                    <audio id="audio:${i.userId}" controls disablecontrols autoplay audioType = 'ambient' audioCategory = 'playback' style="display:none;"></audio>
                 `
             }
         }
@@ -29,13 +29,13 @@ const audioDoms = (userList,userId)=>{
 }
 const audioDoms2 = (userId)=>{
     let html = `
-        <audio id="audio:${userId}" class="firstAudio" controls disablecontrols autoplay></audio>
+        <audio id="audio:${userId}" class="firstAudio" controls disablecontrols autoplay audioType = 'ambient' audioCategory = 'playback' style="display:none;"></audio>
     `
     return html
 }
 const myAudioDom = ()=>{
     let html = `
-        <audio id="myAudio" class="firstAudio" controls disablecontrols autoplay></audio>
+        <audio id="myAudio" class="firstAudio" controls disablecontrols autoplay audioType = 'ambient' audioCategory = 'playback' style="display:none;"></audio>
     `
     return html
 }
